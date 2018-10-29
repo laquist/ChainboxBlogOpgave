77bf8621d9b609db5dc66f07f1fca81863fdd935
Lars Lynnerup <lars9865@edu.campusvejle.dk>
2018-10-29 11:59:40 +0100

    Igang med at lave knappen til at indtaste et image URL.

--- src/js/test.js
+++ src/js/test.js
@@ -1,1 +1,27 @@
 console.log('test');
+
+(function () {
+    let element = document.getElementById('imageInputBtn');
+    console.log(element);
+    console.log('hej');
+
+    element.addEventListener('click', function () {
+        let imgURL = prompt("Please enter the image URL");
+        let input;
+
+        if (imgURL) {
+            input = imgURL;
+            console.log(input);
+        } else {
+            input = "No image chosen";
+            console.log('User cancelled the prompt.')
+            console.log(input);
+        }
+
+        if (input) {
+            let outputElement = document.getElementById('imageInput');
+            outputElement.textContent = input;
+
+        }
+    })
+})();
